691744b4dcd08433cb8c5906d751a06729c1bd88
Richard Afro <richyafro@hotmail.com>
2019-03-24 13:22:04 +0100

    Add products file

---
+++ products.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    "name": "Sharwarma",
+    "price": 23.54,
+    "image": "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?cs=srgb&dl=burrito-chicken-close-up-461198.jpg&fm=jpg",
+    "description": "Fresh hot sharwarma"
+  },
+  {
+    "name": "Burger",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?cs=srgb&dl=biriyani-chicken-cooked-1624487.jpg&fm=jpg",
+    "description": "Mouth watering burger. Who cares if it's healthy"
+  },
+  {
+    "name": "Great dish",
+    "price": 12.13,
+    "image": "https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?cs=srgb&dl=close-up-cooking-dinner-46239.jpg&fm=jpg",
+    "description": "Healthy meal served with a side of protein"
+  },
+  {
+    "name": "Pancakes",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/357573/pexels-photo-357573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=400",
+    "description": "A great tower of pancakes. Dig in!"
+  },
+  {
+    "name": "Meat!",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?cs=srgb&dl=beef-cooking-delicious-769289.jpg&fm=jpg",
+    "description": "Great meat dish for strong healthy bones"
+  },
+  {
+    "name": "Coconut drink",
+    "price": 10.33,
+    "image": "https://images.pexels.com/photos/1030973/pexels-photo-1030973.jpeg?cs=srgb&dl=berries-berry-chia-1030973.jpg&fm=jpg",
+    "description": "Refreshing cocunut drink with strawberries"
+  }
+]
